040d584500f3381cce5ff7a402308bdebf8847b5
vipin rawat <rawat.vipin4818@gmail.com>
2022-06-20 21:54:20 +0530

    build fix

--- components/elements/index.ts
+++ components/elements/index.ts
@@ -1,5 +1,2 @@
 export { default as Pager } from './Pager';
-export { default as Modal } from './Modal';
-export { default as Input } from './Input';
 export { default as Page } from './Page';
-export { default as Tabs } from './Tabs';
